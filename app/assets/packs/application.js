91508e5b6c208b657b0a8441294bebbea638a76f
Alexandr Orlov <morozhunce@gmail.com>
2018-11-02 05:16:18 +0200

    Registration form

--- app/assets/packs/application.js
+++ app/assets/packs/application.js
@@ -1,1 +1,14 @@
 import 'bootstrap/dist/js/bootstrap'
+import '@fortawesome/fontawesome-free/js/all'
+
+let App = App || {};
+
+import Rails from 'rails-ujs'
+import Flash from './flash'
+
+window.App  = App;
+
+Rails.start();
+
+App.flash = Flash;
+Flash.start();
