47acdc34f5dd735e4096c80671ab1d247ef0a50f
Alexandr Orlov <morozhunce@gmail.com>
2018-11-02 15:22:19 +0200

    Stock CRUD implemented

--- app/assets/packs/application.js
+++ app/assets/packs/application.js
@@ -3,12 +3,18 @@ import '@fortawesome/fontawesome-free/js/all'
 
 let App = App || {};
 
-import Rails from 'rails-ujs'
-import Flash from './flash'
+import Rails     from 'rails-ujs'
+import Flash     from './flash'
+import Chartkick from "chartkick";
+import Chart     from "chart.js";
 
-window.App  = App;
+window.App       = App;
+window.Chartkick = Chartkick;
 
 Rails.start();
 
+Chartkick.addAdapter(Chart);
+
 App.flash = Flash;
+
 Flash.start();
---
+++ app/facades/stocks/show_facade.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Stocks
+  class ShowFacade
+    delegate :name, :interest, :duration, :unit_price, to: :stock, prefix: true, allow_nil: true
+
+    def initialize(stock_id:)
+      @stock_id = stock_id
+    end
+
+    def stock
+      @stock ||= Stock.find_by(id: stock_id)
+    end
+
+    def chart_params
+      Stocks::CompoundInterest.call(stock_id: stock.id)
+    end
+
+    private
+
+    attr_reader :stock_id
+  end
+end
